45f07b6030dac2a07ceee2f31ff0350722d5146b
Thadius <thadiusp@gmail.com>
2019-02-05 09:19:33 -0500

    Add gconnect to server

--- vagrant/catalog/server.py
+++ vagrant/catalog/server.py
@@ -5,11 +5,140 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Genre, Movies, Base
 
+#Oauth imports
+from flask import session as login_session
+import random, string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+CLIENT_ID = json.loads(open('client_secret.json', 'r').read())['web']['client_id']
+
 #Connect to database and create the session
 engine = create_engine('sqlite:///moviegenre.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+#Create token and store in login session
+@app.route('/login')
+def showLogin():
+  state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+  login_session['state'] = state
+  return render_template('login.html', state=state)
+
+@app.rout('/gconnect', methods=['POST'])
+def gconnect():
+  if request.args.get('state') != login_session['state']:
+    return jsonify('Invalid state parimeter'), 401
+  #Obtain authorization code
+  code = request.database
+
+  try:
+    #Upgrade authorization code to a credentials object
+    oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
+    oauth_flow.redirect_uri = 'postmessage'
+    credentials = oauth_flow.step2_exchange(code)
+  except FlowExchangeError:
+    return jsonify('Failed to upgrade the authorization code'), 401
+
+  #Check the validity of the access_code
+  access_token = credentials.access_token
+  url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+  h = httplib2.Http()
+  result = json.loads(h.request(url, 'GET')[1].decode('utf-8'))
+
+  #Abort if access token error
+  if result.get('error') is not None:
+    return jsonify('Error'), 500
+
+  #Verify access token is used for intended user
+  google_id = credentials.id_token['sub']
+  if result['user_id'] != google_id:
+    return jsonify('Token users id does not match given user id'), 401
+
+  #Verify app validity for this access token
+  if result['issued_to'] != CLIENT_ID:
+    return jsonify('Token client ID does not match App'), 401
+
+  #Check if user is already logged in
+  stored_credentials = login_session.get('access_token')
+  stored_google_id = login_session.get('goole_id')
+  if stored_credentials is not None and google_id == stored_google_id:
+    return jsonify('Current user is already connected'), 200
+
+  #Store the access token for later session usage
+  login_session['access_token'] = credentials.access_token
+  login_session['google_id'] = google_id
+
+  #Gather Users info
+  userinfo_url = "https://www.googleapis.com/oauth2/v3/userinfo"
+  params = {'access_token': credentials.access_token, 'alt':'json'}
+  answer = requests.get(userinfo_url, params=params)
+  data = answer.json()
+
+  login_session['username'] = data['name']
+  login_session['picture'] = data['picture']
+  login_session['email'] = data['email']
+
+  user_id = getUserID(login_session['email'])
+  if not user_id:
+    user_id = createUser(login_session)
+  login_session['user_id'] = user_id
+
+  output = ''
+  output += '<h1>Welcome'
+  output += login_session['username']
+  output += '!</h1>'
+  output += '<img src="'
+  output += login_session['picture']
+  output += '" style="width: 150px; height: 150px; border-radius: 150px; -webkit-border-radius: 150px; -moz=border-radius: 150px;">'
+  flash("You are now logged in as %s" % login_session['username'])
+  return output
+
+#Create a user
+def createUser(login_session):
+  newUser = User(name=login_session['username'], email=login_session['email'], picture=login_session['picture'])
+  session.add(newUser)
+  session.commit()
+  user = session.query(User).filter_by(email=login_session['email']).one()
+  return user.id
+
+def getUserInfo(user_id):
+  user = session.query(User).filter_by(id=user_id).one()
+  return user
+
+def getUserID(email):
+  try:
+    user = session.query(User).filter_by(email=email).one()
+    return user
+  except:
+    return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Homepage (Shows all genres)
 @app.route('/')
