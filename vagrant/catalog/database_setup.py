5f93a10493a987d9000acb5dffcb9379c10d494c
Thadius <thadiusp@gmail.com>
2019-02-05 10:21:01 -0500

    Fix user tablename

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -5,54 +5,58 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
-  id = Column(Integer, primary_key=True)
-  name = Column(String(250), nullable=False)
-  email = Column(String(250), nullable=False)
-  picture = Column(String(250))
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
-  @property
-  def serialize(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'emai': self.email,
-      'picture': self.picture
-    }
-    
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture
+        }
+
+
 class Genre(Base):
-  __tablename__ = 'genre'
-  id = Column(Integer, primary_key=True)
-  type = Column(String(250), nullable=False)
+    __tablename__ = 'genre'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(250), nullable=False)
 
-  @property
-  def serialize(self):
-    return {
-      'id': self.id,
-      'type': self.type
-    }
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'type': self.type
+        }
+
 
 class Movies(Base):
-  __tablename__ = 'movies'
-  id = Column(Integer, primary_key=True)
-  title = Column(String(250), nullable=False)
-  year = Column(String(4))
-  plot = Column(String(1000))
-  poster = Column(String(250))
-  type = Column(String(80), ForeignKey('genre.type'))
-  genre = relationship(Genre)
-  user_id = Column(String(250), ForeignKey('user.id'))
-  user = relationship(User)
+    __tablename__ = 'movies'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    year = Column(String(4))
+    plot = Column(String(1000))
+    poster = Column(String(250))
+    type = Column(String(80), ForeignKey('genre.type'))
+    genre = relationship(Genre)
+    user_id = Column(String(250), ForeignKey('user.id'))
+    user = relationship(User)
 
-  @property
-  def serialize(self):
-    return {
-      'id': self.id,
-      'title': self.title,
-      'year': self.year,
-      'plot': self.plot,
-      'poster': self.poster
-    }
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'year': self.year,
+            'plot': self.plot,
+            'poster': self.poster
+        }
 
 
 engine = create_engine('sqlite:///moviegenre.db')
